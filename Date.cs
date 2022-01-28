da6e3678d094179ba4d1a727b139b5b6a88e9554
ivand <ivand@DESKTOP-UDVSLL3>
2022-01-28 23:51:05 +0300

    Добавьте файлы проекта.

---
+++ Date.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ScheduleLibrary
+{
+	public class Date
+	{
+		public static DateTime GetFirstSeptember()
+		{
+			if (DateTime.Now.Month < 8)
+				return new DateTime(DateTime.Now.Year - 1, 9, 1);
+			else
+				return new DateTime(DateTime.Now.Year, 9, 1);
+		}
+		public static DateTime GetFistJanuary()
+		{
+			if (DateTime.Now.Month < 8)
+				return new DateTime(DateTime.Now.Year, 1, 1);
+			else
+				return new DateTime(DateTime.Now.Year + 1, 1, 1);
+		}
+	}
+}
---
+++ Lesson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScheduleLibrary
+{
+	public class Lesson
+	{
+		public static Dictionary<string, int> GetQuantityForWeeks(double percent, int lesson)
+		{
+			int countReady = 0;
+			double chance = percent;
+			int weekCount = 1;
+			Dictionary<string, int> weeks = new Dictionary<string, int>();
+			while (countReady < lesson)
+			{
+				int countLesson = (int)Math.Truncate(chance);
+				if (countLesson >= 1) weeks.Add($"week{weekCount}", countLesson);
+				else weeks.Add($"week{weekCount}", countLesson);
+
+				if (chance > 1) chance = chance - Math.Truncate(chance) + percent;
+				else chance += percent;
+
+				countReady += countLesson;
+				weekCount++;
+			}
+			return weeks;
+		}
+	}
+}
